1dda0441cf2db22c674ecde3b0c0406cad19dba3
ashishchourasiya2828 <ashishchourasiya2828@gmail.com>
2025-03-17 01:44:05 +0530

    feat: implement form parsing for product creation with error handling

--- backend/src/controllers/product.contoller.js
+++ backend/src/controllers/product.contoller.js
@@ -3,5 +3,17 @@ const formidable = require('formidable');
 module.exports.createProduct = async (req, res,next) => {
     const form  = new formidable({keepExtensions:true});
 
-    form.parse()
+    form.parse(req,async(err,files,fields)=>{
+        if(err){
+            return res.status(400).json({
+                error:"Error parsing the form "
+            });
+        }
+
+        try {
+            
+        } catch (error) {
+            
+        }
+    })
 }
