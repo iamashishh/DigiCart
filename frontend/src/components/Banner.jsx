6537f29ef74c779060d9621e5400f42452d8a2d8
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-03-17 01:04:45 +0530

    fix: adjust margin for Banner component to improve layout

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="w-full h-[30vh] flex items-center justify-between  max-sm:px-2 px-20 bg-[#FCF0E4FF] ">
+    <div className="w-full h-[30vh] mt-[6%] flex items-center justify-between  max-sm:px-2 px-20 bg-[#FCF0E4FF] ">
       <div className="flex flex-col gap-5">
         <h1 className="w-[16vw] font-archivo text-[22px] leading-[32px]  max-sm:text-sm  max-sm:leading-normal max-sm:w-[45vw] max-sm:font-normal  font-bold text-[#046664]">
           Grab Upto 50% Off On Selected Headphone
