0e107507500f8896d3d4782c4ed363fa02c50070
Harshwardhan Patil <harshwardhanpatil340@gmail.com>
2025-03-27 20:25:27 +0530

    feat: update Account component to display user information and improve logout functionality

--- frontend/src/components/sections/Account.jsx
+++ frontend/src/components/sections/Account.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../../store/Reducers/AuthReducer";
 
 const Account = () => {
   const [isHovered, setIsHovered] = useState(false);
   const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth.usertoken);
 
   return (
     <div
@@ -13,15 +14,24 @@ const Account = () => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <Link
-        to="/login"
-        className="flex items-center gap-1  hover:text-green-800 transition-colors duration-200"
-      >
-        <i className="ri-user-3-line text-xl"></i>
-        <span className="max-sm:hidden lg:block max-md:block md:block">
-          Login
-        </span>
-      </Link>
+      {auth?.token ? (
+        <div className="flex items-center gap-2 cursor-pointer">
+          <i className="ri-user-3-line text-xl"></i>
+          <span className="font-semibold text-green-700">
+            {auth.user?.username || "User"}
+          </span>
+        </div>
+      ) : (
+        <Link
+          to="/login"
+          className="flex items-center gap-1 hover:text-green-800 transition-colors duration-200"
+        >
+          <i className="ri-user-3-line text-xl"></i>
+          <span className="max-sm:hidden lg:block max-md:block md:block">
+            Login
+          </span>
+        </Link>
+      )}
 
       {isHovered && (
         <div
@@ -29,45 +39,54 @@ const Account = () => {
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
-          <div className="flex items-center justify-around">
-            <h1>New customer?</h1>
-            <Link to="/register" className="text-blue-500">
-              Sign Up
-            </Link>
-          </div>
-          <hr className="border-t-1 my-2 border-zinc-300" />
-          <ul className=" flex flex-col gap-2 font-semibold text-lg p-2">
-            <li>
-              <Link to="/profile" className="py-1 px-2 hover:text-green-800">
-                <i className=" mr-2 ri-account-circle-2-line"></i>Profile
-              </Link>
-            </li>
-            <li>
-              <Link to="" className="py-1 px-2 hover:text-green-800">
-                <i className=" mr-2 ri-box-1-line"></i>Orders
-              </Link>
-            </li>
-            <li>
-              <Link to="" className="py-1 px-2 hover:text-green-800">
-                <i className=" mr-2 ri-heart-3-line"></i>Wishlist
-              </Link>
-            </li>
-            <li>
-              <Link to="" className="py-1 px-2 hover:text-green-800">
-                <i className=" mr-2 ri-settings-2-line"></i>Settings
-              </Link>
-            </li>
-            <li>
-              <Link to="/login"
-                onClick={() => {
-                  dispatch(logout());
-                }}
-                className="py-1 px-2 hover:text-green-800"
-              >
-                <i className=" mr-2 ri-logout-box-line"></i>Logout
-              </Link>
-            </li>
-          </ul>
+          {auth?.token ? (
+            <>
+              <div className="flex items-center justify-around">
+                <h1 className="text-gray-600">Hello, {auth.user?.name}</h1>
+              </div>
+              <hr className="border-t-1 my-2 border-zinc-300" />
+              <ul className="flex flex-col gap-2 font-semibold text-lg p-2">
+                <li>
+                  <Link to="/profile" className="py-1 px-2 hover:text-green-800">
+                    <i className="mr-2 ri-account-circle-2-line"></i>Profile
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/orders" className="py-1 px-2 hover:text-green-800">
+                    <i className="mr-2 ri-box-1-line"></i>Orders
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/wishlist" className="py-1 px-2 hover:text-green-800">
+                    <i className="mr-2 ri-heart-3-line"></i>Wishlist
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/settings" className="py-1 px-2 hover:text-green-800">
+                    <i className="mr-2 ri-settings-2-line"></i>Settings
+                  </Link>
+                </li>
+                <li>
+                  <button
+                    onClick={() => dispatch(logout())}
+                    className="py-1 px-2 hover:text-green-800 flex items-center w-full text-left"
+                  >
+                    <i className="mr-2 ri-logout-box-line"></i>Logout
+                  </button>
+                </li>
+              </ul>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-around">
+                <h1>New customer?</h1>
+                <Link to="/register" className="text-blue-500">
+                  Sign Up
+                </Link>
+              </div>
+              <hr className="border-t-1 my-2 border-zinc-300" />
+            </>
+          )}
         </div>
       )}
     </div>
