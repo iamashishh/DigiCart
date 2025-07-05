e3f5e1a43f2955b0c8648db96e1c1ca45504391b
lokendrasingh27 <lokendrarajput07@gmail.com>
2025-07-05 19:37:27 +0530

    done

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -43,7 +43,7 @@ const Register = () => {
     <div className="min-h-screen flex items-center justify-center  lg:px-4  md:px-8 ">
       <div className="relative">
         {/* Left Section: Sign Up Form */}
-        <div className="bg-white lg:p-8 max-sm:p-2 max-md:p-8  p-2 flex flex-col justify-center md:absolute md:w-[25vw] md:h-[70vh] md:top-9 md:-left-40 md:rounded-xl rounded-t-xl shadow-2xl">
+        <div className="bg-white lg:p-8  max-sm:p-2 max-md:p-6    p-2 flex flex-col justify-center md:absolute md:w-[25vw] md:h-[70vh] md:top-9 md:-left-40 md:rounded-xl rounded-t-xl shadow-2xl">
           <h2 className="text-3xl md:text-4xl font-bold text-[#046664] text-center">
             Sign Up
           </h2>
@@ -54,9 +54,9 @@ const Register = () => {
 
           {error && <p className="text-red-500 text-center mt-2">{error}</p>}
 
-          <form className="mt-5" onSubmit={handleSubmit}>
+          <form className="lg:mt-5 max-sm:mt-3 max-md:mt-5 " onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4">
-              <div className="mb-4">
+              <div className="lg:mb-4 max-md:mb-4 max-sm:mb-2">
                 <label
                   htmlFor="username"
                   className="font-semibold text-lg text-[#046664] px-1"
@@ -74,7 +74,7 @@ const Register = () => {
               </div>
             </div>
 
-            <div className="mb-4">
+            <div className="lg:mb-4 max-md:mb-4 max-sm:mb-2">
               <label
                 htmlFor="email"
                 className="font-semibold text-lg text-[#046664] px-1"
@@ -91,7 +91,7 @@ const Register = () => {
               />
             </div>
 
-            <div className="mb-4">
+            <div className="lg:mb-4 max-md:mb-4 max-sm:mb-2">
               <label
                 htmlFor="password"
                 className="font-semibold text-lg text-[#046664] px-1"
@@ -108,7 +108,7 @@ const Register = () => {
               />
             </div>
 
-            <div className="mb-4">
+            <div className="lg:mb-4 max-md:mb-4 max-sm:mb-4">
               <label
                 htmlFor="role"
                 className="font-semibold text-lg text-[#046664] px-1"
@@ -131,7 +131,7 @@ const Register = () => {
 
             <button
               type="submit"
-              className="w-full bg-[#046664] hover:opacity-90 font-semibold text-white py-3 rounded-lg mt-3"
+              className="w-full bg-[#046664] hover:opacity-90 font-semibold text-white py-3 rounded-lg lg:mb-2 "
               disabled={loading}
             >
               {loading ? "Processing..." : "SIGN UP"}
@@ -143,7 +143,7 @@ const Register = () => {
         <div className="bg-[url('./bg.jpeg')] flex flex-col items-center justify-center bg-cover bg-top text-white lg:p-8 max-sm:p-4 max-md:p-4 w-full md:w-[30vw] md:h-[80vh] md:ml-[40%] md:rounded-xl rounded-b-xl shadow-2xl">
           <div className="text-center">
             <h2 className="text-2xl md:text-4xl font-bold">Welcome Back!</h2>
-            <p className="md:mt-4 mt-2 mb-8 text-lg font-semibold md:px-6">
+            <p className="md:mt-4 lg:mt-2 lg:mb-8 max-sm:mb-4 max-md:mb-6 text-lg font-semibold md:px-6">
               To keep connected with us, please log in to your account
             </p>
 
